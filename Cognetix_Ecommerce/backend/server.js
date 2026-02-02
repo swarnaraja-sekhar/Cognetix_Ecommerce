82e88e9ac2a217146ef5fc346a48a35d7c47c60e
SWARNA <bhuchiki12@gmail.com>
2026-02-02 21:02:52 +0530

    Add Vercel app URL to CORS middleware

--- Cognetix_Ecommerce/backend/server.js
+++ Cognetix_Ecommerce/backend/server.js
@@ -33,7 +33,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'https://cognetix-ecommerce.vercel.app'],
   credentials: true,
 }));
 app.use(express.json());
